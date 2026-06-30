8127828848ac1afb551edd438d48bdb4d7cc3ea8
aidaj-png <aidajaidane@gmail.com>
2026-06-30 15:10:29 +0200

    Update inpi_api.py

--- src/inpi_api.py
+++ src/inpi_api.py
@@ -1,16 +1,13 @@
-"""
-Connexion à l'API INPI
-Version 0.1
-"""
-
-import requests
-
-
 class INPIApi:
 
     def __init__(self):
+
         self.base_url = "https://api-gateway.inpi.fr/services/apidiffusion/api"
 
     def test(self):
-        print("Connexion à :", self.base_url)
+
+        print("API INPI")
+
+        print(self.base_url)
+
         return True
