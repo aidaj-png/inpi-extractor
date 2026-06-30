c3ed0f1ba6dfff6686ba8dad603f9a69ed4d62e3
aidaj-png <aidajaidane@gmail.com>
2026-06-30 15:15:27 +0200

    Construction du client INPI

--- src/inpi_api.py
+++ src/inpi_api.py
@@ -1,13 +1,63 @@
+import requests
+
+
 class INPIApi:
+
+    BASE_URL = "https://api-gateway.inpi.fr/services/apidiffusion/api"
 
     def __init__(self):
 
-        self.base_url = "https://api-gateway.inpi.fr/services/apidiffusion/api"
+        self.session = requests.Session()
 
-    def test(self):
+        self.session.headers.update({
+            "Accept": "application/xml",
+            "Content-Type": "application/json"
+        })
 
-        print("API INPI")
+    def build_query(
+        self,
+        word,
+        position=0,
+        size=100,
+        collections=None
+    ):
 
-        print(self.base_url)
+        if collections is None:
+            collections = ["FR", "EU", "WO"]
 
-        return True
+        return {
+
+            "collections": collections,
+
+            "facetsList": [
+                "APPLICANT",
+                "CLASSIFICATION"
+            ],
+
+            "fields": [
+                "ApplicationNumber",
+                "Mark",
+                "MarkCurrentStatusCode",
+                "DEPOSANT",
+                "ukey"
+            ],
+
+            "position": position,
+
+            "query": f"[Mark={word}]",
+
+            "size": size,
+
+            "sortList": [
+                "APPLICATION_DATE DESC",
+                "MARK ASC"
+            ],
+
+            "withCTMRevendication": False,
+
+            "withFacets": True
+        }
+
+    def endpoint(self):
+
+        return f"{self.BASE_URL}/marques/search"
