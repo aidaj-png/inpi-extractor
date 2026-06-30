36eb4ac90e788f09911049b18d1da706cb0052f1
aidaj-png <aidajaidane@gmail.com>
2026-06-30 14:56:54 +0200

    Ajout du fichier de configuration

---
+++ src/config.py
@@ -0,0 +1,19 @@
+"""
+Configuration de l'application INPI Extractor
+"""
+
+# Recherche
+SEARCH_TERM = "France"
+
+# Période de dépôt
+DATE_START = "2017-06-01"
+DATE_END = "2027-06-30"
+
+# Collections interrogées
+COLLECTIONS = ["FR", "EU", "WO"]
+
+# Nombre de résultats par page
+PAGE_SIZE = 100
+
+# Export
+OUTPUT_FILE = "marques_france.xlsx"
