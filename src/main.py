0a43bbe51b74691cbd77cef0925b81a4153cb1c0
aidaj-png <aidajaidane@gmail.com>
2026-06-30 15:22:20 +0200

    Affichage de la requête HTTP

--- src/main.py
+++ src/main.py
@@ -5,27 +5,13 @@ from inpi_api import INPIApi
 
 
 def main():
-
     print("=" * 60)
     print("INPI EXTRACTOR")
     print("=" * 60)
 
     api = INPIApi()
 
-    payload = api.build_query(
-        word=SEARCH_TERM,
-        position=0,
-        size=100
-    )
-
-    print("\nPayload envoyé à l'API :\n")
-
-    pprint(payload)
-
-    print("\nEndpoint :")
-
-    print(api.endpoint())
-
+    api.print_request(SEARCH_TERM)
 
 if __name__ == "__main__":
     main()
